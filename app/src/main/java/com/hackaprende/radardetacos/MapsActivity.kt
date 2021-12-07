e385a93fb5efdcb7513f7d96b307e8b0ec55adb0
manuel <navajaspedro26@gmail.com>
2021-12-06 23:02:57 -0600

    Initial commit

---
+++ app/src/main/java/com/hackaprende/radardetacos/MapsActivity.kt
@@ -0,0 +1,158 @@
+package com.hackaprende.radardetacos
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Location
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2000
+private const val DEFAULT_MAP_SCALE = 13.0f
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private val taquerias = mutableListOf<Taqueria>()
+    private lateinit var tacoIcon: BitmapDescriptor
+    private val userLocation = Location("")
+    private lateinit var myLocationButton: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        myLocationButton = findViewById(R.id.my_location_button)
+        taquerias.add(Taqueria("Tacos de Asada", -33.954044, 151.241283))
+        taquerias.add(Taqueria("Tacos de Pastor", -33.967154, 151.264715))
+        taquerias.add(Taqueria("Tacos de Cochinita", -33.9438208,151.2436039))
+        taquerias.add(Taqueria("Tacos de Barbacoa", -33.936577, 151.259410))
+        taquerias.add(Taqueria("Tacos de Birria", -33.9362557,151.2392932))
+        taquerias.add(Taqueria("Burritos", -33.938594, 151.224316))
+
+        tacoIcon = getTacoIcon()
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            getUserLocation()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                userLocation.latitude = location.latitude
+                userLocation.longitude = location.longitude
+                setupMap()
+            }
+        }
+    }
+
+    private fun setupMap() {
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getUserLocation()
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showLocationPermissionRationaleDialog()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showLocationPermissionRationaleDialog() {
+        val dialog = AlertDialog.Builder(this)
+                .setTitle(R.string.need_location_permission_dialog_title)
+                .setMessage(R.string.need_location_permission_dialog_message)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            LOCATION_PERMISSION_REQUEST_CODE)
+                }.setNegativeButton(R.string.no) { _, _ ->
+                    finish()
+                }
+        dialog.show()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val userLatLng = LatLng(userLocation.latitude, userLocation.longitude)
+        val userMarker = MarkerOptions().position(userLatLng)
+        mMap.addMarker(userMarker)
+
+        for (taqueria in taquerias) {
+            val tacoPosition = LatLng(taqueria.latitude, taqueria.longitude)
+            val tacoLocation = Location("")
+            tacoLocation.latitude = taqueria.latitude
+            tacoLocation.longitude = taqueria.longitude
+            val distanceToTaco = tacoLocation.distanceTo(userLocation)
+            val tacoMarkerOptions = MarkerOptions()
+                    .icon(tacoIcon)
+                    .position(tacoPosition)
+                    .title(taqueria.name)
+                    .snippet(getString(R.string.distance_to_format, distanceToTaco))
+            mMap.addMarker(tacoMarkerOptions)
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, DEFAULT_MAP_SCALE))
+
+        myLocationButton.setOnClickListener {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, DEFAULT_MAP_SCALE))
+        }
+    }
+
+    private fun getTacoIcon(): BitmapDescriptor {
+        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_taco)
+        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(drawable?.intrinsicWidth ?: 0,
+                drawable?.intrinsicHeight ?: 0, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable?.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+}
